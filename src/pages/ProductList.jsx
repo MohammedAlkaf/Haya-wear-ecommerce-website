e68741c545055c01dbb9566beebdc1351a9aabbf
Mohammed Al-Kaf <m.alkaff11@gmail.com>
2022-01-31 22:50:01 -0500

    Product Lists and Filters

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import Anouncement from '../components/Anouncement';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import Newsletter from '../components/Newsletter';
+import Products from '../components/Products';
+
+const ProductList = () => {
+    return(
+        <Contaier>
+            <Navbar/>
+            <Anouncement/>
+            <Title>Dresses</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>
+                        Filter Products:
+                    </FilterText>
+                    <Select>
+                        <Option disabled selected> Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected> Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                        <Option>XXL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>
+                        Sort Products:
+                    </FilterText>
+                    <Select>
+                        <Option disabled selected> Newest</Option>
+                        <Option>Lowest to Heighst Price</Option>
+                        <Option>Highest to Lowest Price</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+        </Contaier>
+    );
+};
+
+const Contaier = styled.div``;
+
+const Title = styled.h1`
+    margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    margin: 20px;
+`;
+
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`;
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`;
+
+const Option = styled.option``;
+
+export default ProductList;
